9bb235e3c197d10a66548a0a972abd9573a57527
David Pham <35905105+basespl1tyt@users.noreply.github.com>
2021-06-09 17:54:34 -0400

    New Website Upload

---
+++ assets/js/header.js
@@ -0,0 +1,33 @@
+    document.write( `
+    <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.html#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
+          <li><a class="nav-link scrollto" href="index.html#features">Features</a></li>
+          <li class="dropdown"><a href="#pricing"><span>Airports</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="phli.html">PHLI - Lihue Airport</a></li>
+              <li><a href="cykf.html">CYKF - Waterloo Airport</a></li>
+              <li><a href="cnc4.html">CNC4 - Guelph Airpark</a></li>
+              <li><a href="kpvu.html">KPVU - Provo Muni</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="index.html#testimonials">Testinomials</a></li>
+          <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="index.html#recent-blog-posts">News</a></li>
+
+          <li><a class="getstarted scrollto" href="https://secure.simmarket.com/regdesigns.mhtml">Buy Now</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+    `);
